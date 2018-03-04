a497eb5d9bb4afecf2943a56af42785efea44419
Mario Esteban Viapiano <marioeviapiano@gmail.com>
2018-03-04 10:36:30 -0300

    Extract HomePage widget to its own separate file

---
+++ lib/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:weighttracker/add_entry_dialog.dart';
+import 'package:weighttracker/model/weight_save.dart';
+import 'package:weighttracker/weight_list_item.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<WeightSave> weightSaves= new List();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new ListView.builder(
+          itemCount: weightSaves.length,
+          itemBuilder: (BuildContext context, int index) {
+            WeightSave weightSave = weightSaves[index];
+            double difference = weightSaves.first == weightSave
+                ? 0.0
+                : weightSave.weight - weightSaves[weightSaves.indexOf(weightSave) - 1].weight;
+            return new WeightListItem(weightSave, difference);
+          }
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _openAddEntryDialog,
+        tooltip: 'Add new weight entry',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _openAddEntryDialog() {
+    Navigator.of(context).push(new MaterialPageRoute<Null>(
+        builder: (BuildContext context) {
+          return new AddEntryDialog();
+        },
+        fullscreenDialog: true
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:weighttracker/add_entry_dialog.dart';
-import 'package:weighttracker/model/weight_save.dart';
-import 'package:weighttracker/weight_list_item.dart';
+import 'package:weighttracker/home_page.dart';
 
 void main() => runApp(new MyApp());
 
@@ -18,51 +14,5 @@ class MyApp extends StatelessWidget {
       ),
       home: new HomePage(title: 'Weight Tracker'),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => new _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<WeightSave> weightSaves= new List();
-  
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new ListView.builder(
-        itemCount: weightSaves.length,
-        itemBuilder: (BuildContext context, int index) {
-          WeightSave weightSave = weightSaves[index];
-          double difference = weightSaves.first == weightSave
-              ? 0.0
-              : weightSave.weight - weightSaves[weightSaves.indexOf(weightSave) - 1].weight;
-          return new WeightListItem(weightSave, difference);
-        }
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _openAddEntryDialog,
-        tooltip: 'Add new weight entry',
-        child: new Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _openAddEntryDialog() {
-    Navigator.of(context).push(new MaterialPageRoute<Null>(
-      builder: (BuildContext context) {
-        return new AddEntryDialog();
-      },
-      fullscreenDialog: true
-    ));
   }
 }
