6c4454dd0e667acb2210ed11c403dfe4199890c0
Mario Esteban Viapiano <marioeviapiano@gmail.com>
2018-03-05 22:15:31 -0300

    Add weight picker

--- lib/add_entry_dialog.dart
+++ lib/add_entry_dialog.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:weighttracker/date_time_item.dart';
 import 'package:weighttracker/model/weight_save.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class AddEntryDialog extends StatefulWidget {
 
@@ -12,9 +13,11 @@ class AddEntryDialog extends StatefulWidget {
 
 class _AddEntryDialogState extends State<AddEntryDialog> {
   DateTime _dateTime = new DateTime.now();
+  double _weight;
 
   @override
   Widget build(BuildContext context) {
+
     return new Scaffold(
       appBar: new AppBar(
         title: const Text("New Entry"),
@@ -34,13 +37,45 @@ class _AddEntryDialogState extends State<AddEntryDialog> {
           )
         ],
       ),
-      body: new ListTile(
-        leading: new Icon(Icons.today, color: Colors.grey[500]),
-        title: new DateTimeItem(
-          dateTime: _dateTime,
-          onChanged: (dateTime) => setState(() => _dateTime = dateTime),
-        ),
+      body: new Column(
+        children: [
+          new ListTile(
+            leading: new Icon(Icons.today, color: Colors.grey[500]),
+            title: new DateTimeItem(
+              dateTime: _dateTime,
+              onChanged: (dateTime) => setState(() => _dateTime = dateTime),
+            ),
+          ),
+          new ListTile(
+            leading: new Image.asset(
+              "assets/scale-bathroom.png",
+              color: Colors.grey[500],
+              height: 24.0,
+              width: 24.0
+            ),
+            title: new Text(
+              "$double _weight km",
+            ),
+            onTap: () => _showWeightPicker(context),
+          )
+        ],
       )
     );
+  }
+
+  _showWeightPicker(BuildContext context) {
+    showDialog(
+      context: context,
+      child: new NumberPickerDialog.decimal(
+        minValue: 1,
+        maxValue: 150,
+        initialDoubleValue: _weight,
+        title: new Text("Enter your weight"),
+      ),
+    ).then((value) {
+      if(value != null) {
+        setState(() => _weight = value);
+      }
+    });
   }
 }
